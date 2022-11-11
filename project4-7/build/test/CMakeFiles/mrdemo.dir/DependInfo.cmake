c23fadb1b564665754b795395ff82274f7b71979
merovingie <rimonmikkhael@gatech.edu>
2022-11-10 21:17:49 -0500

    commit from my gatech repo

---
+++ project4-7/build/test/CMakeFiles/mrdemo.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rimon/project4-7/test/main.cc" "/home/rimon/project4-7/build/test/CMakeFiles/mrdemo.dir/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CARES_STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/include"
+  "src"
+  "/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/rimon/project4-7/build/src/CMakeFiles/mapreducelib.dir/DependInfo.cmake"
+  "/home/rimon/project4-7/build/src/CMakeFiles/p4protolib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
